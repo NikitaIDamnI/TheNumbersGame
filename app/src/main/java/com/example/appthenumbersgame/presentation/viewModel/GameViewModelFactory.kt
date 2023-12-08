21dbdd69b980732c64188fff40b229cb64b5f657
Nikita B <xdnik96@gmail.com>
2023-12-08 18:40:21 +0500

    version 0.2.0.0 creation ViewModelFactory

---
+++ app/src/main/java/com/example/appthenumbersgame/presentation/viewModel/GameViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.example.appthenumbersgame.presentation.viewModel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.appthenumbersgame.domain.entities.Level
+class GameViewModelFactory(
+    private val application: Application,
+    private val level: Level
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(GameViewModel::class.java)) {
+            return GameViewModel(application, level) as T
+        }
+       throw RuntimeException("Unknown view model class $modelClass")
+    }
+
+}
